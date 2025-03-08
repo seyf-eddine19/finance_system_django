987178372fdc7e1be1dae5d71d97dac6bc5eb573
seyf-eddine19 <seyfeddine.tlm@gmail.com>
2025-03-08 20:04:30 +0100

    UPDATE

--- finance_system/settings.py
+++ finance_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1']
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split()
 
 # Application definition
 INSTALLED_APPS = [
