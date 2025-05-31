dd8a517528686186231032689d561868f12088a2
Seyf Eddine <84080839+seyf-eddine19@users.noreply.github.com>
2025-05-31 16:13:05 +0100

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.index, name='index'),
 
     # Loan URLs
