2455c01579221808d7be8ae3590d1f0a8da49af0
Seyf Eddine <84080839+seyf-eddine19@users.noreply.github.com>
2025-05-31 16:12:39 +0100

    Add files via upload

--- finance_system/urls.py
+++ finance_system/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
 
 from django.conf import settings
